96d1c7c1cd9f34599fce898f9ca041e362f0b792
Phyolization <52733299+phyolization@users.noreply.github.com>
2022-01-18 14:04:42 +0800

    Update steeringWeight_Loss.m

--- steeringWeight_Loss.m
+++ steeringWeight_Loss.m
@@ -6,31 +6,28 @@ function [SW, SigLam] = steeringWeight_Loss(sigma,varargin)
 %   remaining two dimensions are (a,x), such that sigma(:,:,a,x) =
 %   \sigma_a|x. 
 %
-% SW = steeringWeight(sigma) returns the steering weight SW of the
-% assemblage sigma.
+% SW = steeringWeight_Loss(sigma,He) returns the steering weight SW of the
+% assemblage sigma with loss counted.
 %
-% This function has one optional argument:
-%   He: (default 1)
+% This function has one additional argument:
+%   He: the Heralding efficiency
 %
 % ->requires: CVX (http://cvxr.com/cvx/), QETLAB (http://www.qetlab.com)
+% ->requires: steeringreview (https://git.io/vax96)
 % ->authors: Paul Skrzypczyk, Daniel Cavalcanti (Modified by Qiang Z)
 % ->last updated: Jan 10, 2021
 
-[He,of] = opt_args({1,1},varargin{:});
-% if unspecified, it is assumed the standard steering weight is required.
-% He: Heralding efficiency
-% of: Optimization factor
+[He] = opt_args({1,1},varargin{:});
 
 [dB,~,oa,ma] = size(sigma);
 % dB = dim. of Bob, oa = # outcomes for Alice, ma = # inputs for Alice
-pri=oa+1; %the priori scenario
-Ndet = (pri)^ma; %number of deterministic behaviours
+pri=oa+1; % the priori scenario
+Ndet = (pri)^ma; % number of deterministic behaviours
 
 SingleParty = zeros(pri,ma,Ndet); % initialise array
 for lam = 0:Ndet-1
-    lamdec = dec2base(lam,pri,ma)-'0'; % generates the string of outcomes a 
-                                %(for each x), for the given variable lam
-                                %将每个字符的ascii码减去‘0’的ascii码即获得该字符串的数列
+    lamdec = dec2base(lam,pri,ma)-'0'; 	% generates the string of outcomes a 
+										%(for each x), for the given variable lam
     for x = 1:ma
         for a = 0:pri-1
 ;           SingleParty(a+1,x,lam+1) = (lamdec(x) == a);...   +(lamdec(x) == a).*(1-He); %
@@ -48,28 +45,28 @@ for i = 1:ma
         end
     end
 end
+% Transform the deterministic array into repeated matrices form
 
 
-% check that the assemblage is valid
 if  NSAssemblage(sigma) == 0
-    error('assemblage is not valid')
+    error('assemblage is not valid')  % check that the assemblage is valid
 end
 
 sigR = sum(sigma(:,:,:,1),3);
 Sigma=sigma.*He;
 Sigma(:,:,pri,:)=repmat(sigR.*(1-He),[1,1,1,ma]);
-% sigR = reduced state of Bob, necessary for consistent steering robust.
+% sigR = reduced state of Bob
 
-% NOTE: Here we use the dual formulation of the steering weight.
+% NOTE: Here we use the primal formulation of the steering weight.
 
 cvx_begin sdp quiet
+
+    if oa>2, cvx_solver sedumi, end
     
     variable SigLam(dB,dB,Ndet) hermitian semidefinite
     % members of the steering functional
 
     maximise real(sum(reshape(SigLam.*repmat(eye(dB),[1,1,Ndet]),1,[])))
-    % sum(reshape(X.*conj(Y),1)) equals to the Trace!!! (if Y is hermitian)
-    % Fascinating!!!
     
     subject to
  
